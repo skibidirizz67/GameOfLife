18a642f5d2fa789c5b2ecc412fcdf57cf9bb74b5
skibidirizz67 <gitskibidirizz@proton.me>
2025-09-22 03:29:45 +0300

    restricted pointers, sleep

--- gol.c
+++ gol.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAP_W 1024
 #define MAP_H MAP_W
 #define PW (MAP_W+2)
 #define PH (MAP_H+2)
+
 #define STEPS 1024
+#define SLEEP 100000
 
 uint8_t map_a[PW*PH], map_b[PW*PH];
+uint8_t *curr, *next;
 
-uint8_t *curr;
-uint8_t *next;
-
-void map_update() {
+void map_update(uint8_t *__restrict cur, uint8_t *__restrict nxt) {
 	for (int y = 1; y < MAP_H; y++) {
 		for (int x = 1; x < MAP_W; x++) {
-			int n = curr[(y-1)*PW+x-1] + curr[(y-1)*PW+x] + curr[(y-1)*PW+x+1]
-			      + curr[y*PW+x-1] + curr[y*PW+x+1]
-				  + curr[(y+1)*PW+x-1] + curr[(y+1)*PW+x] + curr[(y+1)*PW+x+1];
-			next[y*PW+x] = (n==3) | (curr[y*PW+x] & (n==2));
+			int n = cur[(y-1)*PW+x-1] + cur[(y-1)*PW+x] + cur[(y-1)*PW+x+1]
+			      + cur[y*PW+x-1] + cur[y*PW+x+1]
+				  + cur[(y+1)*PW+x-1] + cur[(y+1)*PW+x] + cur[(y+1)*PW+x+1];
+			nxt[y*PW+x] = (n==3) | (cur[y*PW+x] & (n==2));
 		}
 	}
 }
@@ -28,8 +29,8 @@ void map_draw() {
     char buffer[(MAP_W+1) * MAP_H + 1];
     char *bp = buffer;
 
-    for (int y = 1; y <= MAP_H; ++y) {
-        for (int x = 1; x <= MAP_W; ++x) {
+    for (int y = 1; y < MAP_H; y++) {
+        for (int x = 1; x < MAP_W; x++) {
             *bp++ = curr[y*PW+x]? '#' : '.';
         }
         *bp++ = '\n';
@@ -40,26 +41,27 @@ void map_draw() {
     fflush(stdout);
 }
 
-int main() {
-    curr = map_a;
+void map_init() {
+	curr = map_a;
     next = map_b;
 
-    int center = PH/2*PW+PW/2;
-
+	int center = PH/2*PW+PW/2;
     curr[center-PW] = 1;
     curr[center-1] = 1;
     curr[center] = 1;
     curr[center+PW] = 1;
     curr[center+PW+1] = 1;
+}
+
+int main() {
+    map_init();
 
     for (int step = 0; step < STEPS; step++) {
-        map_update();
-        //map_draw();
-
-        uint8_t *tmp = curr;
-        curr = next;
-        next = tmp;
+        map_update(curr, next);
+		//map_draw()
+        uint8_t *tmp = curr; curr = next; next = tmp;
+		//usleep(SLEEP);
     }
-    
+
     return 0;
 }
